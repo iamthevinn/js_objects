7fdb688b660a113ca553e0dafead15460315d291
Vince Carini <vincecarini@northwesternmutual.com>
2018-01-09 15:02:35 -0600

    Done with 1

--- jsObjects.js
+++ jsObjects.js
@@ -1,3 +1,16 @@
+// Name: Vince Carini
+
+// Challenge 1
+
+/*
+Name: Remy, Cohort: Jan
+Name: Genevieve, Cohort: March
+Name: Chuck, Cohort: Jan
+Name: Osmund, Cohort: June
+Name: Nikki, Cohort: June
+Name: Boris, Cohort: June
+*/
+
 let students = [
     {name: 'Remy', cohort: 'Jan'},
     {name: 'Genevieve', cohort: 'March'},
@@ -6,3 +19,35 @@ let students = [
     {name: 'Nikki', cohort: 'June'},
     {name: 'Boris', cohort: 'June'}
 ];
+
+for(let key in students) {
+    console.log("Name: " + students[key].name + ", Cohort: " + students[key].cohort);
+}
+
+// Challenge 2
+
+/*
+EMPLOYEES
+1 - JONES, MIGUEL - 11
+2 - BERTSON, ERNIE - 12
+3 - LU, NORA - 6
+4 - BARKYOUMB, SALLY - 14
+MANAGERS
+1 - CHAMBERS, LILLIAN - 15
+2 - POE, GORDON - 9
+*/
+
+let users = {
+    employees: [
+        {'first_name':  'Miguel', 'last_name' : 'Jones'},
+        {'first_name' : 'Ernie', 'last_name' : 'Bertson'},
+        {'first_name' : 'Nora', 'last_name' : 'Lu'},
+        {'first_name' : 'Sally', 'last_name' : 'Barkyoumb'}
+    ],
+    managers: [
+       {'first_name' : 'Lillian', 'last_name' : 'Chambers'},
+       {'first_name' : 'Gordon', 'last_name' : 'Poe'}
+    ]
+ };
+
+

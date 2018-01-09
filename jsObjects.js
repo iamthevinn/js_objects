22a4ee5642378ef1e45c796b0a87cf17199ea834
Vince Carini <vincecarini@northwesternmutual.com>
2018-01-09 14:44:51 -0600

    first commit

---
+++ jsObjects.js
@@ -0,0 +1,8 @@
+let students = [
+    {name: 'Remy', cohort: 'Jan'},
+    {name: 'Genevieve', cohort: 'March'},
+    {name: 'Chuck', cohort: 'Jan'},
+    {name: 'Osmund', cohort: 'June'},
+    {name: 'Nikki', cohort: 'June'},
+    {name: 'Boris', cohort: 'June'}
+];
